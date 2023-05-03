a65ed84447f1147e136018dba3b09b063a2b9a42
wasu <jjnovo.alumno@iesrafaelalberti.es>
2023-05-03 09:28:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { Icon } from "@iconify/vue"
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <div class="userIcon">
+      <Icon icon="mdi:user" class="user"/>
+    </div>
+  </header>
+
+  <RouterView />
+
+  <footer>
+    <nav class="socialmedia">
+      <Icon icon="mdi:twitter" class="icon"/>
+      <Icon icon="ph:instagram-logo-fill" class="icon"/>
+      <Icon icon="ic:baseline-facebook" class="icon"/>
+    </nav>
+    <nav class="infocookies">
+      <p>Informacion</p>
+      <p>Cookies</p>
+    </nav>
+  </footer>
+</template>
+
+<style scoped>
+  header{
+    background-color: #696969;
+    width: 100%;
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+    min-height: 57px;
+  }
+  nav{
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+  .userIcon{
+    width: 26px;
+    height: 26px;
+    background-color: #D9D9D9;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+  }
+  .user{
+    color: #696969;
+  }
+  footer{
+    background-color: #D9D9D9;
+    min-height: 42px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+  .icon{
+    width: 22px;
+    height: 22px;
+  }
+  .socialmedia{
+    width: 30%;
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+  }
+  .infocookies{
+    width: 50%;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,84 @@
+<script setup>
+  import { Icon } from "@iconify/vue"
+</script>
+<template>
+  <main>
+    <div class="filter">
+      <div class="buscador">
+        <input type="text" placeholder="Busque tatuadores!">
+      </div>
+      <div class="estilos">
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+      </div>
+      <div v-if="mostrar" class="estilos" :disabled="mostrar">
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+        <label>Japones<input type="checkbox" class="check"></label>
+      </div>
+      <label class="more" v-if="!mostrar"><Icon icon="material-symbols:expand-more"/><input type="checkbox" v-model="mostrar" class="check"></label>
+      <label class="more" v-if="mostrar"><Icon icon="material-symbols:expand-less" /><input type="checkbox" v-model="mostrar" class="check"></label>
+    </div>
+  </main>
+</template>
+<script>
+  export default{
+    name: "Home",
+    data: () => ({
+      mostrar:false
+    })
+  }
+</script>
+<style scoped>
+  main{
+    min-height: 750px;
+  }
+  .filter{
+    background-color: #D9D9D9;
+    width: 100%;
+    min-height: 132px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  input{
+    border-radius: 10px;
+  }
+  .check[type="checkbox"]{
+    display: none;
+  }
+  label{
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    margin: 6px;
+    width: 20%;
+    text-align: center;
+  }
+  .more {
+    background-color: #FFFFFF;
+    border-radius: 20px;
+    margin: 6px;
+    width: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .estilos{
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
